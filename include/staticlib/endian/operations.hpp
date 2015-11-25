58996882b559d71d21ce5c0ecd348cb2cf1d87b3
alex <alex@staticlibs.net>
2015-11-25 13:47:41 +0000

    initial commit

---
+++ include/staticlib/endian/operations.hpp
@@ -0,0 +1,190 @@
+/*
+ * Copyright 2015, alex at staticlibs.net
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* 
+ * File:   operations.hpp
+ * Author: alex
+ *
+ * Created on November 21, 2015, 9:09 PM
+ */
+
+#ifndef STATICLIB_ENDIAN_OPERATIONS_HPP
+#define	STATICLIB_ENDIAN_OPERATIONS_HPP
+
+#include <cstdint>
+
+#include "staticlib/io/operations.hpp"
+
+#include "staticlib/endian/config.hpp"
+
+namespace staticlib {
+namespace endian {
+
+/**
+ * Reads 2-byte little-endian integer from the specified source
+ * 
+ * @param src source
+ * @return integer read
+ */
+template<typename IntType, typename Source>
+IntType read_16_le(Source& src) {
+    uint16_t res = 0;
+    staticlib::io::read_exact(src, reinterpret_cast<char*> (std::addressof(res)), 2);
+    return static_cast<IntType> (le16toh(res));
+}
+
+/**
+ * Reads 2-byte big-endian integer from the specified source
+ * 
+ * @param src source
+ * @return integer read
+ */
+template<typename IntType, typename Source>
+IntType read_16_be(Source& src) {
+    uint16_t res = 0;
+    staticlib::io::read_exact(src, reinterpret_cast<char*> (std::addressof(res)), 2);
+    return static_cast<IntType> (be16toh(res));
+}
+
+/**
+ * Reads 4-byte little-endian integer from the specified source
+ * 
+ * @param src source
+ * @return integer read
+ */
+template<typename IntType, typename Source>
+IntType read_32_le(Source& src) {
+    uint32_t res = 0;
+    staticlib::io::read_exact(src, reinterpret_cast<char*> (std::addressof(res)), 4);
+    return static_cast<IntType> (le32toh(res));
+}
+
+/**
+ * Reads 4-byte big-endian integer from the specified source
+ * 
+ * @param src source
+ * @return integer read
+ */
+template<typename IntType, typename Source>
+IntType read_32_be(Source& src) {
+    uint32_t res = 0;
+    staticlib::io::read_exact(src, reinterpret_cast<char*> (std::addressof(res)), 4);
+    return static_cast<IntType> (be32toh(res));
+}
+
+/**
+ * Reads 8-byte little-endian integer from the specified source
+ * 
+ * @param src source
+ * @return integer read
+ */
+template<typename IntType, typename Source>
+IntType read_64_le(Source& src) {
+    uint64_t res = 0;
+    staticlib::io::read_exact(src, reinterpret_cast<char*> (std::addressof(res)), 8);
+    return static_cast<IntType> (le64toh(res));
+}
+
+/**
+ * Reads 8-byte big-endian integer from the specified source
+ * 
+ * @param src source
+ * @return integer read
+ */
+template<typename IntType, typename Source>
+IntType read_64_be(Source& src) {
+    uint64_t res = 0;
+    staticlib::io::read_exact(src, reinterpret_cast<char*> (std::addressof(res)), 8);
+    return static_cast<IntType> (be64toh(res));
+}
+
+/**
+ * Writes specified 2-byte integer to the specified sink in little-endian
+ * 
+ * @param sink destination sink
+ * @param val integer to write
+ */
+template<typename IntType, typename Sink>
+void write_16_le(Sink& sink, IntType val) {
+    uint16_t val16 = le16toh(static_cast<uint16_t> (val));
+    staticlib::io::write_all(sink, reinterpret_cast<char*> (std::addressof(val16)), 2);
+}
+
+/**
+ * Writes specified 2-byte integer to the specified sink in big-endian
+ * 
+ * @param sink destination sink
+ * @param val integer to write
+ */
+template<typename IntType, typename Sink>
+void write_16_be(Sink& sink, IntType val) {
+    uint16_t val16 = be16toh(static_cast<uint16_t> (val));
+    staticlib::io::write_all(sink, reinterpret_cast<char*> (std::addressof(val16)), 2);
+}
+
+/**
+ * Writes specified 4-byte integer to the specified sink in little-endian
+ * 
+ * @param sink destination sink
+ * @param val integer to write
+ */
+template<typename IntType, typename Sink>
+void write_32_le(Sink& sink, IntType val) {
+    uint32_t val32 = le32toh(static_cast<uint32_t> (val));
+    staticlib::io::write_all(sink, reinterpret_cast<char*> (std::addressof(val32)), 4);
+}
+
+/**
+ * Writes specified 4-byte integer to the specified sink in big-endian
+ * 
+ * @param sink destination sink
+ * @param val integer to write
+ */
+template<typename IntType, typename Sink>
+void write_32_be(Sink& sink, IntType val) {
+    uint32_t val32 = be32toh(static_cast<uint32_t> (val));
+    staticlib::io::write_all(sink, reinterpret_cast<char*> (std::addressof(val32)), 4);
+}
+
+/**
+ * Writes specified 8-byte integer to the specified sink in little-endian
+ * 
+ * @param sink destination sink
+ * @param val integer to write
+ */
+template<typename IntType, typename Sink>
+void write_64_le(Sink& sink, IntType val) {
+    uint64_t val64 = le64toh(static_cast<uint64_t> (val));
+    staticlib::io::write_all(sink, reinterpret_cast<char*> (std::addressof(val64)), 8);
+}
+
+/**
+ * Writes specified 8-byte integer to the specified sink in big-endian
+ * 
+ * @param sink destination sink
+ * @param val integer to write
+ */
+template<typename IntType, typename Sink>
+void write_64_be(Sink& sink, IntType val) {
+    uint64_t val64 = be64toh(static_cast<uint64_t> (val));
+    staticlib::io::write_all(sink, reinterpret_cast<char*> (std::addressof(val64)), 8);
+}
+
+} // namespace
+}
+
+#endif	/* STATICLIB_ENDIAN_OPERATIONS_HPP */
+
---
+++ test/operations_test.cpp
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2015, alex at staticlibs.net
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* 
+ * File:   operations_test.cpp
+ * Author: alex
+ *
+ * Created on November 25, 2015, 12:25 PM
+ */
+
+#include "staticlib/endian/operations.hpp"
+
+#include <array>
+#include <iostream>
+
+#include "staticlib/io.hpp"
+
+#include "staticlib/config/assert.hpp"
+
+namespace io = staticlib::io;
+namespace en = staticlib::endian;
+
+void test_read_16_le() {
+    io::string_source src{"42"};
+    int16_t val = en::read_16_le<int16_t>(src);
+    slassert(12852 == val);
+}
+
+void test_read_16_be() {
+    io::string_source src{"42"};
+    int16_t val = en::read_16_be<int16_t>(src);
+    slassert(13362 == val);
+}
+
+void test_read_32_le() {
+    io::string_source src{"4256"};
+    int32_t val = en::read_32_le<int32_t>(src);
+    slassert(909455924 == val);
+}
+
+void test_read_32_be() {
+    io::string_source src{"4256"};
+    int32_t val = en::read_32_be<int32_t>(src);    
+    slassert(875705654 == val);
+}
+
+void test_read_64_le() {
+    io::string_source src{"42563178"};
+    int64_t val = en::read_64_le<int64_t>(src);
+    slassert(4050760485865599540ll == val);
+}
+
+void test_read_64_be() {
+    io::string_source src{"42563178"};
+    int64_t val = en::read_64_be<int64_t>(src);
+    slassert(3761127145711155000ll == val);
+}
+
+void test_write_16_le() {
+    io::string_sink sink{};
+    int16_t val = 12852;
+    en::write_16_le(sink, val);
+    slassert("42" == sink.get_string());
+}
+
+void test_write_16_be() {
+    io::string_sink sink{};
+    int16_t val = 13362;
+    en::write_16_be(sink, val);
+    slassert("42" == sink.get_string());
+}
+
+void test_write_32_le() {
+    io::string_sink sink{};
+    int32_t val = 909455924;
+    en::write_32_le(sink, val);
+    slassert("4256" == sink.get_string());
+}
+
+void test_write_32_be() {
+    io::string_sink sink{};
+    int32_t val = 875705654;
+    en::write_32_be(sink, val);
+    slassert("4256" == sink.get_string());
+}
+
+void test_write_64_le() {
+    io::string_sink sink{};
+    int64_t val = 4050760485865599540ll;
+    en::write_64_le(sink, val);
+    slassert("42563178" == sink.get_string());
+}
+
+void test_write_64_be() {
+    io::string_sink sink{};
+    int64_t val = 3761127145711155000ll;
+    en::write_64_be(sink, val);
+    slassert("42563178" == sink.get_string());
+}
+
+int main() {
+    try {
+        test_read_16_le();
+        test_read_16_be();
+        test_read_32_le();
+        test_read_32_be();
+        test_read_64_le();
+        test_read_64_be();
+        test_write_16_le();
+        test_write_16_be();
+        test_write_32_le();
+        test_write_32_be();
+        test_write_64_le();
+        test_write_64_be();
+    } catch (const std::exception& e) {
+        std::cout << e.what() << std::endl;
+        return 1;
+    }
+    return 0;
+}
+
